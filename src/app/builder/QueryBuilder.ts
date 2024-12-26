9a762dab4ad7a6e8b79865a0196628cb536fdbc1
rabby-web <rabby.webdeveloper@gmail.com>
2024-12-26 19:46:05 +0600

    feat: enhance query builder with search, filter, and sorting
    - Added search functionality to filter results based on specified searchable fields.
    - Implemented filter method to handle custom filtering, including author-based filtering.
    - Added sortBy method to sort results based on query parameters, supporting both ascending and descending order.
    - Improved flexibility for querying MongoDB with dynamic search, filtering, and sorting options.

---
+++ src/app/builder/QueryBuilder.ts
@@ -0,0 +1,53 @@
+import { FilterQuery, Query } from 'mongoose';
+
+export class QueryBuilder<T> {
+  public modelQuery: Query<T[], T>;
+  public query: Record<string, unknown>;
+
+  constructor(modelQuery: Query<T[], T>, query: Record<string, unknown>) {
+    this.modelQuery = modelQuery;
+    this.query = query;
+  }
+
+  search(searchableFields: string[]) {
+    if (this?.query?.search) {
+      this.modelQuery = this.modelQuery.find({
+        $or: searchableFields.map((field) => {
+          return {
+            [field]: { $regex: this?.query?.search, $options: 'i' },
+          } as FilterQuery<T>;
+        }),
+      });
+    }
+    return this;
+  }
+
+  filter() {
+    const queryObj = { ...this.query };
+
+    // console.log(queryObj)
+
+    const excludeFields = ['search', 'sortBy', 'sortOrder'];
+
+    excludeFields?.forEach((el) => delete queryObj[el]);
+
+    if (queryObj?.filter) {
+      this.modelQuery = this.modelQuery.find({ author: queryObj.filter });
+      delete queryObj.filter;
+    }
+
+    this.modelQuery = this.modelQuery.find(queryObj as FilterQuery<T>);
+
+    return this;
+  }
+
+  sortBy() {
+    const sortBy =
+      (this?.query?.sortBy as string)?.split(',')?.join(' ') || '-createdAt';
+    const sortOrder = this.query?.sortOrder === 'desc' ? '-' : '';
+
+    this.modelQuery = this.modelQuery.sort(`${sortOrder}${sortBy}`);
+
+    return this;
+  }
+}
