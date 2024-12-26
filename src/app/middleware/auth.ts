6d431b6ff850e3d3fe7bcf7f8d923d8f39e20e2a
rabby-web <rabby.webdeveloper@gmail.com>
2024-12-26 20:58:10 +0600

    feat: add authorization middleware with role-based access control
    - Implemented uth middleware to verify JWT tokens and enforce role-based access control.
    - Checks for the presence of the token, validates it, and decodes the payload.
    - Verifies the existence and status (active/inactive) of the user.
    - Allows for role-based authorization, ensuring the user has the required role to access the route.
    - Throws AppError for unauthorized access or invalid credentials.

---
+++ src/app/middleware/auth.ts
@@ -0,0 +1,52 @@
+import { NextFunction, Request, Response } from 'express';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import catchAsync from '../utils/catchAsync';
+import { StatusCodes } from 'http-status-codes';
+import config from '../config';
+import AppError from '../errors/AppError';
+import User from '../modules/user/user.model';
+import { TUserRole } from '../modules/user/user.interface';
+
+const auth = (...requiredRoles: TUserRole[]) => {
+  return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const token = req.headers.authorization?.split(' ')[1];
+    // checking if the token is missing
+    if (!token) {
+      // throw new Error('You are not authorized!');
+      throw new AppError(StatusCodes.UNAUTHORIZED, 'Invalid credentials');
+    }
+
+    // checking if the given token is valid
+    const decoded = jwt.verify(
+      token,
+      config.jwt_access_secret as string,
+    ) as JwtPayload;
+
+    // console.log({ decoded });
+
+    const { role, email } = decoded;
+
+    // checking if the user is exist
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      throw new AppError(StatusCodes.UNAUTHORIZED, 'Invalid credentials');
+    }
+
+    // checking if the user is inactive
+    const isBlocked = user.isBlocked;
+
+    if (isBlocked) {
+      throw new Error('This user is blocked!');
+    }
+
+    if (requiredRoles && !requiredRoles.includes(role)) {
+      throw new AppError(StatusCodes.UNAUTHORIZED, 'Invalid credentials');
+    }
+
+    req.user = decoded as JwtPayload;
+    next();
+  });
+};
+
+export default auth;
